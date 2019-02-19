3fb192a71891876ad242efd25bf569f7a52475fb
tmattellis <tmattellis@DESKTOP-BMS8JU0.localdomain>
2019-02-19 15:20:04 -0600

    Changes 2/19

--- dl_filter_align_hpv.py
+++ dl_filter_align_hpv.py
@@ -27,7 +27,6 @@ python tool.py Sample.1.fq Sample.2.fq ref.fa [numCPUS]
 # left to do:
 # check errors in cmd function
 # properly redirect stdout
-# set up Github repo
 
 # change language for help descriptions
 # clean up variable names
@@ -52,7 +51,7 @@ def cmd(args, write=False, filepath=None):
         try:
             subp.check_call(args, stdout=sys.stdout)
         except subp.CalledProcessError, e:
-            print(e.returncode)
+            print("Subprocesss error with code: " + e.returncode)
             return;
         except:
             print("An unknown error occurred")
@@ -65,7 +64,7 @@ def cmd(args, write=False, filepath=None):
         try:
             subp.check_call(args)
         except subp.CalledProcessError, e:
-            print(e.returncode)
+            print("Subprocesss error with code: " + e.returncode)
             return;
         except:
             print("An unknown error occurred")
@@ -79,7 +78,7 @@ def main():
     myparse.add_argument('sampleName', metavar="sampleName", help="name of the sample(s) to be aligned")
     myparse.add_argument('reference', metavar="refFasta", help="the reference .fa file for the program")
     myparse.add_argument('path', metavar="path", help="path to the Human Genome")
-    myparse.add_argument('-cpus', type=int, default=4, help="number of CPUS for processing")
+    myparse.add_argument('-cpus', type=int, default=2, help="number of CPUS for processing")
     myparse.add_argument('-twosamps', type=bool, default=False, help="number of sample fq files")
 
     args = myparse.parse_args()
@@ -88,64 +87,67 @@ def main():
 
     topdirectory = os.getcwd()
 
+    nameOnly = args.sampleName.split(".")[0]
+
     if(args.twosamps == True):
         numSamples = 2
     else:
         numSamples = 1
             
 
-   #cmd(["mkdir", args.sampleName])
+    cmd(["mkdir", nameOnly])
     
-   # don't really need, just modify paths
-   # cmd(["cd", args.sampleName])
-    cmd(["echo", "Extracting raw reads"]) # if bam file given as input, convert to fastq
+    # don't really need, just modify paths
+    # cmd(["cd", args.sampleName])
+    # with cd(args.sampleName): # to change directories
             
          
     # # generates 2 fastq files (need to add option for just one)
-    # if(args.sampleName.lower().endswith(".bam")):
-    #     cmd(["samtools", "fastq",
-    #          "-1{}.1.fq".format(args.sampleName),
-    #          "-2{}.2.fq".format(args.sampleName),
-    #          "-0{}".format(os.devnull), 
-    #          "-n", "-F 0x900", "-@ {}".format(args.cpus-1),
-    #          "{}".format(args.sampleName)])
+    if(args.sampleName.lower().endswith(".bam")):
+        cmd(["echo", "Extracting raw reads"]) # if bam file given as input, convert to fastq
+        cmd(["samtools", "fastq",
+             "-1{}.1.fq".format(args.sampleName),
+             "-2{}.2.fq".format(args.sampleName),
+             "-0{}".format(os.devnull), 
+             "-n", "-F 0x900", "-@ {}".format(args.cpus-1),
+             "{}".format(args.sampleName)])
 
-    # cmd(["echo", "Aligning reads to human genome"])
+    cmd(["echo", "Aligning reads to human genome"])
 
-    # ##generates Aligned.out.bam, Chimeric.out.junction, Log.final.out, Log.out
-    # ##Log.progress.out, SJ.out.tabl, Unmapped.out.mate1, Unmapped.out.mate2
+    ##generates Aligned.out.bam, Chimeric.out.junction, Log.final.out, Log.out
+    ##Log.progress.out, SJ.out.tabl, Unmapped.out.mate1, Unmapped.out.mate2
 
-    # cmd(
-    #     ["STAR", 
-    #     "--genomeDir HumanGenome",#/gscmnt/gc6127/research/m.inkman/genome_ref/GRCh38_r90/genomeDir",
-    #     "--readFilesIn {sampleName}.1.fq {sampleName}.2.fq".format(sampleName=args.sampleName),
-    #     "--runThreadN {}".format(args.cpus),
-    #     "--chimSegmentMin 18",
-    #     "--outSAMtype BAM Unsorted",
-    #     "--outReadsUnmapped Fastx",
-    #     "--outFilterMultimapNmax 100",
-    #     "--outFileNamePrefix ./{}.".format(args.sampleName)])
+    cmd(
+        ["STAR", 
+        "--genomeDir HumanGenome",#/gscmnt/gc6127/research/m.inkman/genome_ref/GRCh38_r90/genomeDir",
+        "--readFilesIn {sampleName}.1.fq {sampleName}.2.fq".format(sampleName=args.sampleName),
+        "--runThreadN {}".format(args.cpus),
+        "--chimSegmentMin 18",
+        "--outSAMtype BAM Unsorted",
+        "--outReadsUnmapped Fastx",
+        "--outFilterMultimapNmax 100",
+        "--outFileNamePrefix ./{}.".format(args.sampleName)])
 
-    # for format in {'*.out','*.junction','*.tab','*.Aligned.*'}:
-    #     for file in glob.glob(format):
-    #         os.remove(file)
+    for format in {'*.out','*.junction','*.tab','*.Aligned.*'}:
+        for file in glob.glob(format):
+            os.remove(file)
 
 
-    # cmd(
-    #     ["rm", 
-    #     "{}.1.fq".format(args.sampleName), 
-    #     "{}.2.fq".format(args.sampleName)])
+    cmd(
+        ["rm", 
+        "{}.1.fq".format(args.sampleName), 
+        "{}.2.fq".format(args.sampleName)])
 
-    # cmd(["echo", "Aligning reads to HPV genomes"])
+    cmd(["echo", "Aligning reads to HPV genomes"])
 
     for i in range(1, numSamples + 1):
-    #     cmd(
-    #         ["bwa", 
-    #         "aln", 
-    #         "-t {}".format(args.cpus),
-    #         "{}".format(args.reference),
-    #         "{sampleName}.Unmapped.out.mate{i}".format(sampleName=args.sampleName, i=i)], 
-    #         True, "{sampleName}.{i}.sai".format(sampleName=args.sampleName, i=i))
+        cmd(
+            ["bwa", 
+            "aln", 
+            "-t {}".format(args.cpus),
+            "{}".format(args.reference),
+            "{sampleName}.Unmapped.out.mate{i}".format(sampleName=args.sampleName, i=i)], 
+            True, "{sampleName}.{i}.sai".format(sampleName=args.sampleName, i=i))
 
         cmd(
             ["bwa", 
@@ -161,9 +163,11 @@ def main():
             "-F4"
             "-@ {}".format((args.cpus - 1)),
             "{sampleName}.{i}.aln-se.sam".format(sampleName=args.sampleName, i=i)], 
-            True,"{topdir}/HPV.aligned.{i}.sam".format(topdir=topdirectory, sampleName=args.sampleName, i=i))
-                          #{sampleName}directory/
-        # cmd(["rm", "*.sai", "*.sam"])
+            True,"{topdir}/{name}/HPV.aligned.{i}.sam".format(topdir=topdirectory, name=nameOnly, i=i))
+
+        for format in {'*.sam','*.sai'}:
+            for file in glob.glob(format):
+                os.remove(file)
 
 
 if __name__ == '__main__':
